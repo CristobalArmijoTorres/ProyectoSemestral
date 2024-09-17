a0efc4546c938be0af1444dfa42a5daf840c865a
CristobalArmijoTorres <141270818+CristobalArmijoTorres@users.noreply.github.com>
2024-09-17 20:20:18 -0300

    Falta el diseño :)
    EL usuario es Usuario1
    La clave es MiClav3

--- src/app/usuario/usuario.page.ts
+++ src/app/usuario/usuario.page.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.page.html',
   styleUrls: ['./usuario.page.scss'],
 })
 export class UsuarioPage implements OnInit {
+  mostrar = false;
+  usuarioIngresado = ''; 
+  usuario = '';
+  claveIngresada = '';
+  clave = '';
 
-  constructor() { }
+
+  constructor(private toastController:ToastController) { }
 
   ngOnInit() {
   }
+  async ingresar()
+  {
+    this.usuarioIngresado = this.usuario;
+    this.claveIngresada = this.clave;
 
+    if(this.usuarioIngresado !== 'Usuario1' && this.claveIngresada !== 'Usuario1')
+    {
+        const toast = await this.toastController.create({
+        message:'El usuario o la contraseña ingresada no es correcta, Intentalo nuevamente' ,
+        duration: 3000,
+        position:"middle", // top, middle
+        color : "danger",
+      });
+      await toast.present();
+    }
+    else
+    {
+      const toast = await this.toastController.create({
+        message:'Ingresando......' ,
+        duration: 3000,
+        position:"middle", // top, middle
+        color : "success",
+      });
+      await toast.present();
+    }
+  }
 }
