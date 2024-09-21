1493e257a22a84273d94c71ff31c204edfb3fb9f
CristobalArmijoTorres <141270818+CristobalArmijoTorres@users.noreply.github.com>
2024-09-21 14:26:36 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/app/asistencias/asistencias.page.ts
+++ src/app/asistencias/asistencias.page.ts
@@ -6,10 +6,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./asistencias.page.scss'],
 })
 export class AsistenciasPage implements OnInit {
+  asistenciaArq = [
+    {asignatura : "Arquitectura", fecha: '02-09-2024', presente: true},
+    {asignatura : "Arquitectura", fecha: '04-09-2024', presente: true},
+    {asignatura : "Arquitectura", fecha: '06-09-2024', presente: false},
+    {asignatura : "Arquitectura", fecha: '09-09-2024', presente: false},
+    {asignatura : "Arquitectura", fecha: '11-09-2024', presente: true},
+    {asignatura : "Arquitectura", fecha: '13-09-2024', presente: false},
+  ]
+  asistenciaCDS = [
+    {asignatura : "Calidad De Software", fecha: '02-09-2024', presente: true},
+    {asignatura : "Calidad De Software", fecha: '04-09-2024', presente: true},
+    {asignatura : "Calidad De Software", fecha: '06-09-2024', presente: false},
+    {asignatura : "Calidad De Software", fecha: '09-09-2024', presente: false},
+    {asignatura : "Calidad De Software", fecha: '11-09-2024', presente: true},
+    {asignatura : "Calidad De Software", fecha: '13-09-2024', presente: false},
+  ]
+  asistenciaED = [
+    {asignatura : "Estadística Descriptiva", fecha: '02-09-2024', presente: true},
+    {asignatura : "Estadística Descriptiva", fecha: '04-09-2024', presente: true},
+    {asignatura : "Estadística Descriptiva", fecha: '06-09-2024', presente: false},
+    {asignatura : "Estadística Descriptiva", fecha: '09-09-2024', presente: false},
+    {asignatura : "Estadística Descriptiva", fecha: '11-09-2024', presente: true},
+    {asignatura : "Estadística Descriptiva", fecha: '13-09-2024', presente: false},
+  ]
+  asistenciaEPET = [
+    {asignatura : "Ética Para El Trabajo", fecha: '02-09-2024', presente: true},
+    {asignatura : "Ética Para El Trabajo", fecha: '04-09-2024', presente: true},
+    {asignatura : "Ética Para El Trabajo", fecha: '06-09-2024', presente: false},
+    {asignatura : "Ética Para El Trabajo", fecha: '09-09-2024', presente: false},
+    {asignatura : "Ética Para El Trabajo", fecha: '11-09-2024', presente: true},
+    {asignatura : "Ética Para El Trabajo", fecha: '13-09-2024', presente: false},
+  ]
+  asistenciaII = [
+    {asignatura : "Ingles Intermedio", fecha: '02-09-2024', presente: true},
+    {asignatura : "Ingles Intermedio", fecha: '03-09-2024', presente: true},
+    {asignatura : "Ingles Intermedio", fecha: '04-09-2024', presente: false},
+    {asignatura : "Ingles Intermedio", fecha: '05-09-2024', presente: false},
+    {asignatura : "Ingles Intermedio", fecha: '06-09-2024', presente: true},
+    {asignatura : "Ingles Intermedio", fecha: '09-09-2024', presente: false},
+  ]
+  asistenciaPDP = [
+    {asignatura : "Proceso De Portafolio", fecha: '12-08-2024', presente: true},
+    {asignatura : "Proceso De Portafolio", fecha: '16-09-2024', presente: true},
+  ]
+
+  permitir1: Boolean = false;
+  permitir2: Boolean = false;
+  permitir3: Boolean = false;
+  permitir4: Boolean = false;
+  permitir5: Boolean = false;
+  permitir6: Boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  mostrar1()
+  {
+    this.permitir1 = !this.permitir1;
+  }
+  mostrar2()
+  {
+    this.permitir2 = !this.permitir2;
+  }
+  mostrar3()
+  {
+    this.permitir3 = !this.permitir3;
+  }
+  mostrar4()
+  {
+    this.permitir4 = !this.permitir4;
+  }
+  mostrar5()
+  {
+    this.permitir5 = !this.permitir5;
+  }
+  mostrar6()
+  {
+    this.permitir6 = !this.permitir6;
+  }
+  
 }
