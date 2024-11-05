8977bdb774dbb3819b6603b5bd2c6b3aadd1a5c7
CristobalArmijoTorres <141270818+CristobalArmijoTorres@users.noreply.github.com>
2024-11-04 22:55:31 -0300

    validaciones para profe

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,59 +6,67 @@ import { ToastController, NavController, AlertController } from '@ionic/angular'
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage{
+export class HomePage {
   usuarioIngresado = ''; 
-  usuario = 'Usuario1';
   claveIngresada = '';
+  usuario = 'Usuario1';
   clave = 'MiClav3';
-  constructor(private toastController:ToastController, private navCtrol:NavController, private alertController : AlertController) {}
-  ngOnInit() {
+  usuarioProfe = 'Profesor1';
+  claveProfe = 'Profe123';
 
-  }
+  constructor(
+    private toastController: ToastController, 
+    private navCtrol: NavController, 
+    private alertController: AlertController
+  ) {}
 
-  async funcionAlerta()
-  {
+  ngOnInit() {}
+
+  async funcionAlerta() {
     const alert = await this.alertController.create({
       header: "¿Olvidaste tu contraseña?",
-      subHeader: "Tranquilo, se enviara un mensaje a tu correo para recuperarla",
-      message: "El mensaje se envio correctamente, ahora podras recuperar tu contraseña",
+      subHeader: "Tranquilo, se enviará un mensaje a tu correo para recuperarla",
+      message: "El mensaje se envió correctamente, ahora podrás recuperar tu contraseña",
       buttons: ['Ok']
     });
     await alert.present();
   }
 
-  async ingresar()
-  {
+  async ingresar() {
     this.usuarioIngresado = this.usuarioIngresado;
     this.claveIngresada = this.claveIngresada;
 
-    if(this.usuarioIngresado !== this.usuario ||  this.claveIngresada !== this.clave)
-      {
-          const toast = await this.toastController.create({
-        message:'El usuario o la contraseña ingresada no es correcta, Intentalo nuevamente' ,
-        duration: 3000,
-        position:"middle", 
-        color : "danger",
-      });
-      await toast.present();
-      
-    }
-    else
-    {
-      //LOCALSTORAGE IR A INSPECCIONAR Y SELECCIONAR APPLICATION
+    // Validación para usuario normal
+    if (this.usuarioIngresado === this.usuario && this.claveIngresada === this.clave) {
       localStorage.setItem(this.usuarioIngresado, this.claveIngresada);
       const toast = await this.toastController.create({
-        message:'Ingresando......' ,
+        message: 'Ingresando......',
         duration: 1000,
-        position:"middle", 
-        color : "success",
+        position: "middle", 
+        color: "success",
       });
       await toast.present();
       this.navCtrol.navigateForward('/menu');
+      
+    // Validación para usuario profesor
+    } else if (this.usuarioIngresado === this.usuarioProfe && this.claveIngresada === this.claveProfe) {
+      localStorage.setItem(this.usuarioIngresado, this.claveIngresada);
+      const toast = await this.toastController.create({
+        message: 'Ingresando.....',
+        duration: 1000,
+        position: "middle", 
+        color: "success",
+      });
+      await toast.present();
+      this.navCtrol.navigateForward('#');  
+    } else {
+      const toast = await this.toastController.create({
+        message: 'El usuario o la contraseña ingresada no es correcta, Inténtalo nuevamente',
+        duration: 3000,
+        position: "middle", 
+        color: "danger",
+      });
+      await toast.present();
     }
   }
-
 }
-
-
-
