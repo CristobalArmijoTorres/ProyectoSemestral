2076045572ffde8e8093dfd9fcaa96297ea6f89c
CristobalArmijoTorres <141270818+CristobalArmijoTorres@users.noreply.github.com>
2024-11-09 18:48:18 -0300

    agregar las asignaturas correspndiente
    Falta agregar datos a la modal

--- src/app/asignaturas/asignaturas.page.ts
+++ src/app/asignaturas/asignaturas.page.ts
@@ -1,4 +1,3 @@
-// asignaturas.page.ts
 import { Component, OnInit } from '@angular/core';
 import { AsigAlumnoService } from '../asignaturas/asig-alumno.service';
 import { Asignatura } from './models'; // Importa el modelo
@@ -24,7 +23,6 @@ export class AsignaturasPage implements OnInit {
       this.asigAlumnoService.getAsignaturasByStudentId(storedUser.id).subscribe(
         (asignaturas: Asignatura[]) => {
           console.log('Asignaturas cargadas:', asignaturas);
-          // Asegurarse de que cada asignatura tenga la propiedad `mostrarModal` inicializada en `false`
           this.asignaturas = asignaturas.map(asignatura => ({ ...asignatura, mostrarModal: false }));
         },
         (error: any) => {
--- src/app/asignaturas/models.ts
+++ src/app/asignaturas/models.ts
@@ -1,12 +1,17 @@
-// models.ts
 export interface Asignatura {
   idAsig: string;
   nombre: string;
+  codigo: string;
   mostrarModal?: boolean;
 }
 
 export interface Estudiante {
   estudianteId: string;
+  nombre: string;
+  asignaturas: Asignatura[];
+}
+export interface Asignaturas {
+  profesorId: string;
   nombre: string;
   asignaturas: Asignatura[];
 }
